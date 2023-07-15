4e9f24f15dfb701dbd942221337b3974dd5c0033
Dezycraft <dezyangel29@yahoo.com>
2023-07-15 13:58:29 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*main - Program that prints with a new line
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return 0;
+}
