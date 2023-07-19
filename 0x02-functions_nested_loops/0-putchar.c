ca1860aa637b0a3cf0ca743dc3c63eaae26764bc
Dezycraft <dezyangel29@yahoo.com>
2023-07-19 12:42:22 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - A function that prints _putchar
+ *
+ * Return: Always success
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+functions and nested loops, introduction to prototypes and creating header files
